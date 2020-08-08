da99e8d903ad5281f7559308f5e674829dd9b796
mmichelj <migmichel27@gmial.com>
2020-08-07 21:58:19 -0500

    a*

--- catkin_ws/src/simulator/src/motion_planner/motion_planner_node.cpp
+++ catkin_ws/src/simulator/src/motion_planner/motion_planner_node.cpp
@@ -18,13 +18,16 @@
 #include "../state_machines/light_follower.h"
 #include "../state_machines/sm_avoidance.h"
 #include "../state_machines/sm_avoidance_destination.h"
+#include "../state_machines/sm_avoidance_modified.h"
 #include "../state_machines/sm_destination.h"
 #include "../state_machines/user_sm.h"
-#include "../state_machines/dijkstra.h"
-#include "../state_machines/dfs.h"
+//#include "../state_machines/dijkstra.h"
+//#include "../state_machines/dfs.h"
 #include "clips_ros/SimuladorRepresentation.h"
 #include "../behaviors/oracle.h"
 #include "../action_planner/action_planner.h"
+#include "../state_machines/campos_potenciales.h"
+#include "../state_machines/astar.h"
 
 
 int main(int argc ,char **argv)
@@ -52,6 +55,11 @@ int main(int argc ,char **argv)
     int cta_steps;
     int flg_result;
     int flg_noise=0;
+    int vuelta;
+    int bug_sm=0;
+
+    int q_inputs2=0;
+    int stepCounter;
     
     float result;
     float final_x;
@@ -61,6 +69,43 @@ int main(int argc ,char **argv)
     float max_turn_angle;
     float noise_advance;
     float noise_angle;
+
+    float pendiente;
+    float Xo;
+    float Yo;
+    float Xm;
+    float Ym;
+    
+
+    float a = 0;
+    float b = 0;
+    float c = 0;
+
+    float initialPosition[2] = {0,0};
+    float max_light_intensity = 0;
+    bool circledOnce = 0;
+
+    float pos_x;
+    float pos_y;
+    float qx;
+    float qy;
+    float qx0;
+    float qy0;
+    float qxdes;
+    float qydes;
+    float theta;
+    
+
+    /*float pos_history[2][10]={
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };*/
+
+    float pos_history[2]={0,0};
+    float diferencia_x=0;
+    float diferencia_y=0;
+    int cont_similar=0;
+    bool obstacle_flag=0;
     
     char path[100];
     char object_name[20];
@@ -172,7 +217,7 @@ int main(int argc ,char **argv)
 
 
             case 6:
-                // it finds a path from the origen to a destination using depth first search
+              /*  // it finds a path from the origen to a destination using depth first search
                 if(flagOnce)
                 {
                     for(i = 0; i < 200; i++) steps[i].node = -1;
@@ -219,12 +264,12 @@ int main(int argc ,char **argv)
                             }
                         }
                     }
-                }
+                }*/
 
                 break;
 
             case 7:
-                if(flagOnce)
+                /*if(flagOnce)
                 {
                     for(i = 0; i < 200; i++)steps[i].node=-1;
                     // it finds a path from the origen to a destination using the Dijkstra algorithm
@@ -268,7 +313,7 @@ int main(int argc ,char **argv)
                             }
                         }
                     }
-                }
+                }*/
                 break;
 
             case 8:
@@ -293,9 +338,93 @@ int main(int argc ,char **argv)
             
             case 10:
 
-		action_planner(params.robot_x, params.robot_y,params.robot_theta,&movements);
+		        action_planner(params.robot_x, params.robot_y,params.robot_theta,&movements);
 
                 break;
+            
+            case 11:
+            //A* con campos potenciales
+
+                if(flagOnce)
+                {
+                    for(i = 0; i < 200; i++)steps[i].node=-1;
+                    // it finds a path from the origen to a destination using the Dijkstra algorithm
+                    astar(params.robot_x ,params.robot_y ,params.light_x ,params.light_y ,params.world_name,steps);
+                    print_algorithm_graph (steps);
+                    i=0;
+                    final_x=params.light_x;
+                    final_y= params.light_y;
+                    set_light_position(steps[i].x,steps[i].y);
+                    printf("New Light %d: x = %f  y = %f \n",i,steps[i].x,steps[i].y);
+                    flagOnce = 0;
+                    flg_finish=0;
+                    est_sig = 0;
+                    movements.twist=0.0;
+                    movements.advance =0.0;
+                }
+                else
+                {
+
+                    /**********************Campos Potenciales*******************************/
+
+                    //Si no ha cambiado su posición más de .1 en x Y y desde hace 10 iteraciones, salir del obstaculo
+
+                    diferencia_x=fabs(pos_history[0]-params.robot_x);
+                    diferencia_y=fabs(pos_history[1]-params.robot_y);
+
+                    pos_history[0]=params.robot_x;
+                    pos_history[1]=params.robot_y;
+
+                    printf("diferencias: %f %f %d",diferencia_x,diferencia_y,cont_similar);
+
+                    if(cont_similar<30)
+                    {
+                        if(diferencia_x<0.03 && diferencia_y<0.03){
+                        cont_similar++;
+                        } else{
+                        cont_similar=0;
+                        }
+                        flg_result = campos_potenciales(intensity, light_readings,&movements,max_advance,max_turn_angle,lidar_readings,params.laser_num_sensors,params.laser_value,params.laser_origin,params.laser_range);
+                    }
+
+                    if(cont_similar>=30 && cont_similar<40){
+                        sm_avoid_obstacles_m(q_inputs,&movements,&est_sig, params.robot_max_advance * 0.8 ,params.robot_turn_angle * 0.5 ,12);
+                        cont_similar++;
+                    }
+
+                    if(cont_similar==40){
+                        cont_similar=0;
+                        est_sig=0;
+                    }
+
+
+                    /*********************************************************************/
+
+                    if(flg_result == 1)
+                    {
+                        if(flg_finish == 1) stop();
+                        else
+                        {
+                            if(steps[i].node != -1)
+                            {
+                                set_light_position(steps[i].x,steps[i].y);
+                                printf("New Light %d: x = %f  y = %f \n",i,steps[i].x,steps[i].y);
+                                printf("Node %d\n",steps[i].node);
+                                i++;
+                                //printf("type a number \n");
+                                //scanf("%d",&tmp);
+                            }
+                            else
+                            {
+                                set_light_position(final_x,final_y);
+                                printf("New Light %d: x = %f  y = %f \n",i,final_x,final_y);
+                                flg_finish=1;
+                            }
+                        }
+                    }
+                }
+
+            break;
 
 
 
---
+++ catkin_ws/src/simulator/src/state_machines/campos_potenciales.h
@@ -0,0 +1,223 @@
+/***********************************************
+*                                              *
+*      campos_empty.h	                       *
+*                                              *
+*      Diego Cordero                           *
+*      Jesus Savage			                   *
+*      Miguel Michel			               *
+*					                           *
+*                                              *
+*              Bio-Robotics Laboratory         *
+*              UNAM, 2019                      *
+*                                              *
+*                                              *
+************************************************/
+/*Esta funcion recibe los datos de luz de los sensores, encuentra la posición del destino, calcula la fuerza de atracción y mueve al robot*/
+#include <stdio.h> 
+#include <math.h> 
+
+#define THRESHOLD_FOLLOWER 30
+
+ 
+
+int campos_potenciales(float intensity,float *light_values,movement *movements,float max_advance, float max_turn_angle,float *observations, int size, float laser_value, float ang_origen, float ang_final)
+{
+	/*
+      IMPORTANTE: El angulo del sensor de en medio es 0. Los de la izquierda son +. Los de la derecha 2*pi-el angulo.
+    */
+
+//Constantes del comportamiento del robot
+float etha=500;
+float n=0.002; //5
+//
+
+
+ int sensor = 0;
+ int i=0;
+ int result = 0;
+ float thd=0;
+ float Dd=0;
+ float qdest[2]={0,0};
+ float Fatr[2]={0,0};
+ float qn[2]={0,0};
+ float mqn=0;
+ float thqn=0;
+ float mF=0;
+ float max_light_intensity=0;
+
+ float Frep[2]={0,0};
+ float magFrep=0;
+ float d=5;
+ float thobs=0;
+ float qobs[2]={0,0};
+
+ float Ftot[2]={0,0};
+ float mpi=3.1415;
+
+ //borrar
+ float pFrep[2]={0,0};
+ float maxFrep[2]={0,0};
+ float prevFrep[2]={0,0};
+
+ 
+int a,b;
+int iz,de,salida;
+int j;
+int par_flag=0;
+
+iz = de = salida = 0;
+
+ if(intensity > THRESHOLD_FOLLOWER)
+ {
+
+	movements->twist = 0.0;
+ 	movements->advance = 0.0;
+	result = 1;
+	printf("\n **************** Reached light source ******************************\n");
+ }
+ else
+ {
+	 //********** Fuerza de atracción ************//
+	 for(i = 1; i < 8; i++) 
+ 	{
+		//printf("\n **************** Light values %f******************************\n", light_values[i]);
+	    if( light_values[i] > light_values[sensor])
+		sensor = i;
+		max_light_intensity=light_values[sensor];
+ 	}
+ 	
+ 	if(sensor > 4)
+	   sensor = -(8 - sensor);
+
+
+	Dd=0.9979*pow(max_light_intensity, -0.8495);
+	thd = sensor * 3.1315 / 16;
+
+	qdest[0]=Dd*cos(thd);
+	qdest[1]=Dd*sin(thd);
+
+	//printf("\n **************** qdestx=%f qdesty=%f ******************************\n",qdest[0],qdest[1]);
+
+	Fatr[0]=-etha*qdest[0];
+	Fatr[1]=-etha*qdest[1];
+
+	mF=sqrt(pow(Fatr[0],2)+pow(Fatr[1],2));
+	//Fatr[0]=Fatr[0]/mF;
+	//Fatr[1]=Fatr[1]/mF;
+
+	if(Dd<0.05){
+		Fatr[0]=Fatr[0]*50;
+		Fatr[1]=Fatr[1]*50;
+	}
+
+	//********** Fuerza de repulsión ************//
+
+	d=5;
+
+	if( size % 2 != 0)
+    {
+        a = ( size - 1 ) / 2;
+        b = a + 1;
+		par_flag=0;
+    }
+    else
+    {
+        a = b = size / 2;
+		par_flag=1;
+    }
+
+	
+	
+	//Para todas las lecturas de los sensores
+	for(int i=0;i<size;i++){
+		//printf("************** observations[%d]: %f ***************\n", i, observations[i]);
+
+		if(observations[i]<laser_value){
+			//Calcular magnitud de la fuerza de repulsión para cada sensor
+			//La fuerza de repulsion solo se calcula para los sensores que estan dentro del rango
+			
+			//Para obtener qobs_x y qobs_y:
+
+			thobs=ang_origen+i*(ang_origen+ang_final)/size;
+
+			qobs[0]=observations[i]*cos(thobs);
+			qobs[1]=observations[i]*sin(thobs);
+
+			
+			if(Dd>observations[i]){
+				if(fabs(qobs[0])>0.01){
+					pFrep[0]=-n*(1/fabs(qobs[0])-1/d)*(1/(pow(qobs[0],2)));
+					Frep[0]=Frep[0]-n*(1/fabs(qobs[0])-1/d)*(1/(pow(qobs[0],2)));
+					maxFrep[0]= (pFrep[0]<prevFrep[0])?prevFrep[0]:pFrep[0];
+					prevFrep[0]=pFrep[0];
+				}
+				if(fabs(qobs[1])>0.01){
+					Frep[1]=Frep[1]-n*(1/fabs(qobs[1])-1/d)*(1/(pow(qobs[1],2)));
+					pFrep[1]=-n*(1/fabs(qobs[1])-1/d)*(1/(pow(qobs[1],2)));
+					maxFrep[1]= (pFrep[1]<prevFrep[1])?prevFrep[1]:pFrep[1];
+					prevFrep[1]=pFrep[1];
+				}	
+			}
+			if(observations[i]<0.02){
+				Frep[0]=10*Frep[0];
+				Frep[1]=10*Frep[1];
+			}
+
+		}
+	}
+
+	
+
+	float mFrep=0;
+	float mFtot=0;
+	
+	mFrep=sqrt(pow(Frep[0],2)+pow(Frep[1],2));
+
+	if(mFrep<0.01 && mFrep>-0.01){
+		mFrep=1;
+	}
+
+	//Frep[0]=Frep[0]/mFrep;
+	//Frep[1]=Frep[1]/mFrep;
+
+	
+	Ftot[0]=Fatr[0]+Frep[0];
+	Ftot[1]=Fatr[1]+Frep[1];
+
+	mFtot=sqrt(pow(Ftot[0],2)+pow(Ftot[1],2));
+
+	if(mFtot<0.01 && mFtot>-0.01){
+		mFtot=1;
+	}
+
+	Ftot[0]=Ftot[0]/mFtot;
+	Ftot[1]=Ftot[1]/mFtot;
+
+	printf("************** Frep[%d]: (%f,%f) Fatr[%d]=(%f,%f)***************\n\n", i, Frep[0],Frep[1],i,Fatr[0],Fatr[1]);
+
+	printf("******** Ftot[0]=%f Ftot[1]=%f ********", Ftot[0],Ftot[1]);
+
+	//********** Gradient descent ************//
+	qn[0]=-max_advance*Ftot[0];
+	qn[1]=-max_advance*Ftot[1];
+
+
+	//********** Movimiento del robot ************//
+
+	mqn=sqrt(pow(qn[0],2)+pow(qn[1],2));
+	thqn=atan(qn[1]/qn[0]);
+
+	if(mqn<0 && mqn<-max_advance){
+		mqn=-max_advance;
+	} else if(mqn>0 && mqn>max_advance){
+		mqn=max_advance;
+	}
+	
+	movements->twist = thqn;
+ 	movements->advance = mqn;
+ }
+
+ return result;
+
+}
+
